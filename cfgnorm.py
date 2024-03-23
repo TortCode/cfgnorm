d0a9028c73a2d5346d863858c82d3ab144ab4a10
Teerth Patel <teerthp2004@gmail.com>
2024-03-23 18:00:27 -0500

    Update cfgnorm.py

--- cfgnorm.py
+++ cfgnorm.py
@@ -6,7 +6,6 @@ from copy import deepcopy
 
 
 def proper_powerset(iterable):
-    "powerset([1,2,3]) → () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
     s = list(iterable)
     return it.chain.from_iterable(it.combinations(s, r) for r in range(len(s)))
 
